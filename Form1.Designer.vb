daeb595e12c9f42820296543bc0632bdb0102d9b
Rusyadi <rusyadi365@gmail.com>
2024-02-09 14:24:53 +0700

    Added UI / UX And Compress Function

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,4 +1,7 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -22,10 +25,121 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnCompress = New Button()
+        lblFileName = New Label()
+        ofdFile = New OpenFileDialog()
+        imagePreview = New PictureBox()
+        lblDateCreated = New Label()
+        lblFileSize = New Label()
+        titleSelect = New Label()
+        lblHasil = New Label()
+        CType(imagePreview, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnCompress
+        ' 
+        btnCompress.BackColor = Color.White
+        btnCompress.FlatAppearance.BorderColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        btnCompress.FlatStyle = FlatStyle.System
+        btnCompress.ImageAlign = ContentAlignment.TopCenter
+        btnCompress.Location = New Point(146, 442)
+        btnCompress.Name = "btnCompress"
+        btnCompress.Size = New Size(184, 46)
+        btnCompress.TabIndex = 0
+        btnCompress.Text = "Compress Image"
+        btnCompress.UseVisualStyleBackColor = False
+        ' 
+        ' lblFileName
+        ' 
+        lblFileName.AutoSize = True
+        lblFileName.Location = New Point(12, 9)
+        lblFileName.Name = "lblFileName"
+        lblFileName.Size = New Size(0, 25)
+        lblFileName.TabIndex = 1
+        ' 
+        ' ofdFile
+        ' 
+        ofdFile.FileName = "ofdFile"
+        ofdFile.Filter = "Image Files (*.jpg; *.jpeg; *.png; *.gif; *.bmp)|*.jpg; *.jpeg; *.png; *.gif; *.bmp"
+        ' 
+        ' imagePreview
+        ' 
+        imagePreview.BackgroundImageLayout = ImageLayout.None
+        imagePreview.Image = CType(resources.GetObject("imagePreview.Image"), Image)
+        imagePreview.Location = New Point(146, 92)
+        imagePreview.Name = "imagePreview"
+        imagePreview.Size = New Size(532, 331)
+        imagePreview.SizeMode = PictureBoxSizeMode.Zoom
+        imagePreview.TabIndex = 2
+        imagePreview.TabStop = False
+        ' 
+        ' lblDateCreated
+        ' 
+        lblDateCreated.AutoSize = True
+        lblDateCreated.Location = New Point(12, 47)
+        lblDateCreated.Name = "lblDateCreated"
+        lblDateCreated.Size = New Size(17, 25)
+        lblDateCreated.TabIndex = 3
+        lblDateCreated.Text = " "
+        ' 
+        ' lblFileSize
+        ' 
+        lblFileSize.AutoSize = True
+        lblFileSize.Location = New Point(402, 433)
+        lblFileSize.Name = "lblFileSize"
+        lblFileSize.Size = New Size(17, 25)
+        lblFileSize.TabIndex = 4
+        lblFileSize.Text = " "
+        ' 
+        ' titleSelect
+        ' 
+        titleSelect.AutoSize = True
+        titleSelect.BackColor = SystemColors.Control
+        titleSelect.Enabled = False
+        titleSelect.Location = New Point(298, 372)
+        titleSelect.Name = "titleSelect"
+        titleSelect.Size = New Size(182, 25)
+        titleSelect.TabIndex = 5
+        titleSelect.Text = "Silahkan Pilih Gambar"
+        ' 
+        ' lblHasil
+        ' 
+        lblHasil.AutoSize = True
+        lblHasil.Location = New Point(402, 473)
+        lblHasil.Name = "lblHasil"
+        lblHasil.Size = New Size(17, 25)
+        lblHasil.TabIndex = 6
+        lblHasil.Text = " "
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(800, 524)
+        Controls.Add(lblHasil)
+        Controls.Add(titleSelect)
+        Controls.Add(lblFileSize)
+        Controls.Add(lblDateCreated)
+        Controls.Add(imagePreview)
+        Controls.Add(lblFileName)
+        Controls.Add(btnCompress)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Name = "Form1"
+        Text = "Basic Compress Image"
+        CType(imagePreview, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+
+    Friend WithEvents btnCompress As Button
+    Friend WithEvents lblFileName As Label
+    Friend WithEvents ofdFile As OpenFileDialog
+    Friend WithEvents imagePreview As PictureBox
+    Friend WithEvents lblDateCreated As Label
+    Friend WithEvents lblFileSize As Label
+    Friend WithEvents titleSelect As Label
+    Friend WithEvents lblHasil As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,109 @@
-﻿Public Class Form1
+﻿Imports System.Drawing.Imaging
+Imports System.Globalization
+Imports System.IO
 
+Public Class Form1
+
+    Private Sub CompressImage(ByVal filePath As String)
+        Try
+            ' Memuat gambar dari file yang dipilih
+            Dim originalImage As New Bitmap(filePath)
+
+            ' Menyiapkan parameter untuk kompresi gambar
+            Dim encode As ImageCodecInfo = GetEncoderInfo(ImageFormat.Jpeg)
+            Dim encoderParams As New EncoderParameters(1)
+            Dim qualityParam As New EncoderParameter(Encoder.Quality, 50L) ' Mengatur kualitas kompresi (0-100)
+            encoderParams.Param(0) = qualityParam
+
+            ' Menyimpan gambar yang sudah dikompresi
+            Dim newFilePath As String = Path.Combine(Path.GetDirectoryName(filePath), "compressed_" + Path.GetFileName(filePath))
+            originalImage.Save(newFilePath, encode, encoderParams)
+
+            ' Menampilkan pesan bahwa kompresi berhasil
+            Dim fileInfo As New FileInfo(newFilePath)
+            Dim hasil As String = FormatFileSize(fileInfo.Length)
+            lblHasil.Text = $"Compressed File : {hasil}"
+
+            MessageBox.Show($"Kompresi gambar berhasil menjadi {hasil}. Gambar tersimpan di {newFilePath}", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show($"Terjadi kesalahan: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Function GetEncoderInfo(ByVal format As ImageFormat) As ImageCodecInfo
+        Dim codecs As ImageCodecInfo() = ImageCodecInfo.GetImageDecoders()
+
+        For Each codec As ImageCodecInfo In codecs
+            If codec.FormatID = format.Guid Then
+                Return codec
+            End If
+        Next
+
+        Return Nothing
+    End Function
+
+    Private Sub PickImage()
+        ' Membuka dialog untuk memilih file gambar
+
+        If ofdFile.ShowDialog() = DialogResult.OK Then
+            ' Menampilkan nama file yang dipilih pada label
+            lblFileName.Text = $"Nama File : {ofdFile.SafeFileName}"
+
+            Dim fileInfo As New FileInfo(ofdFile.FileName)
+            Dim specificCulture As New CultureInfo("id-ID")
+            Dim readableFormat As String = fileInfo.CreationTime.ToString("dddd, dd MMMM yyyy, HH:mm", specificCulture)
+
+            lblDateCreated.Text = $"Tanggal Dibuat : {readableFormat}"
+
+            ' Retrieve the size of the image file in bytes
+            Dim fileSizeBytes As Long = fileInfo.Length
+
+            ' Convert bytes to megabytes
+            Dim readableSize As String = FormatFileSize(fileSizeBytes)
+            lblFileSize.Text = $"Ukuran File: {readableSize}"
+
+            Dim selectedImage As New Bitmap(ofdFile.FileName)
+            imagePreview.Image = selectedImage
+
+            CheckFile()
+        End If
+    End Sub
+
+    Private Sub imagePreview_Click(sender As Object, e As EventArgs) Handles imagePreview.Click
+        PickImage()
+    End Sub
+
+    Private Sub btnCompress_Click(sender As Object, e As EventArgs) Handles btnCompress.Click
+
+        ' Memanggil procedure untuk melakukan kompresi gambar
+        CompressImage(ofdFile.FileName)
+
+    End Sub
+
+    Private Function FormatFileSize(ByVal fileSizeBytes As Long) As String
+        Dim sizeTypes() As String = {"Bytes", "KB", "MB", "GB", "TB", "PB", "EB"}
+        Dim sizeType As Integer = 0
+        Dim fileSize As Double = fileSizeBytes
+
+        While fileSize >= 1024 And sizeType < sizeTypes.Length - 1
+            fileSize /= 1024
+            sizeType += 1
+        End While
+
+        Return String.Format("{0:0.##} {1}", fileSize, sizeTypes(sizeType))
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckFile()
+    End Sub
+
+    Private Sub CheckFile()
+        If String.IsNullOrEmpty(ofdFile.FileName) Or ofdFile.FileName Is "ofdFile" Then
+            btnCompress.Visible = False
+            titleSelect.Visible = True
+        Else
+            btnCompress.Visible = True
+            titleSelect.Visible = False
+        End If
+    End Sub
 End Class
